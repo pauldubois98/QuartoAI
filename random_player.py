10af7ad2d17769499e29ddf4daaa34e86545ca58
pauldubois98 <pauldubois98@gmail.com>
2023-03-01 00:56:07 +0100

    random playing strategies with pytorch

---
+++ random_player.py
@@ -0,0 +1,21 @@
+import torch
+
+
+def randomPlace(board):
+    available_places = torch.where(board[0, :, :] == 0)
+    i = torch.randint(available_places[0].shape[0], (1,))
+    return available_places[0][i], available_places[1][i]
+
+
+def randomGive(pieces):
+    available_pieces = torch.where(pieces[0, :] == 1)
+    i = torch.randint(available_pieces[0].shape[0], (1,))
+    return available_pieces[0][i]
+
+
+class RandomPlayer:
+    def give(self, board, pieces):
+        return randomGive(pieces)
+
+    def place(self, board, pieces, piece):
+        return randomPlace(board)
